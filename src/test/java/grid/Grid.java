347e145c1089380cf8fc182020821ce1539890b5
Irina20017 <imudryakodessa@gmail.com>
2017-02-26 01:19:09 +0200

    commit  grid 1

--- src/test/java/grid/Grid.java
+++ src/test/java/grid/Grid.java
@@ -39,12 +39,12 @@ public class Grid extends TestBase  {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         loginPage.loginToDms();
         wait.until(jsLoad);
-        Thread.sleep(1000);
+     //   Thread.sleep(1000);
         driver.get(baseUrl+"/dms/auctions/admin#grading");
         gridPage = PageFactory.initElements(driver, AdminGridPage.class);
         wait.until(jsLoad);
         wait.until(jQueryLoad );
-        Thread.sleep(1000);
+      //  Thread.sleep(1000);
 
 
     }
@@ -55,7 +55,7 @@ public class Grid extends TestBase  {
         int virsection = 1;
 
        do {
-           Thread.sleep(2000);
+           Thread.sleep(1000);
             num1 = Integer.toString(virsection);
             gridPage.virSection(num1).click();  // selected Vir section
             Thread.sleep(2000);
@@ -63,83 +63,58 @@ public class Grid extends TestBase  {
 
 
            int size = gridPage.elementsInVirSection(num1).size(); //определить количество элементоа в выпадающем списке VIR секции
-        //   JOptionPane.showMessageDialog(null, "size ="+ size);
+
            int i = 1;
             do {
 
                 num = Integer.toString(i);
                 gridPage.exteriorElement(num, num1).click(); //кликнуть на элемент VIR секции
-                Thread.sleep(1000);
+                Thread.sleep(500);
 
                 List<WebElement> tablesList = gridPage.getTableList(num, num1);
-                int sizerT = tablesList.size(); // оределить количество таблиц
-              //  JOptionPane.showMessageDialog(null, "Количество таблиц в ДОМ ="+ sizerT); //вывод на экран количества рядов
-                WebElement table = tablesList.get(sizerT-1);
-               // JOptionPane.showMessageDialog(null, "последняя таблица ="+ table);
-
-                id = table.getAttribute("id");
-             //   JOptionPane.showMessageDialog(null, "id таблицы ="+ id);
-
-
+                int sizerT = tablesList.size(); // оределить количество таблиц в <li>
+                WebElement table = tablesList.get(sizerT-1); //последняя таблица в <li>
+                id = table.getAttribute("id"); //id таблицы
                 int sizer = gridPage.getTableRows(id); // оределить количество рядов в открывшейся таблице
-              //  JOptionPane.showMessageDialog(null, "rows ="+ sizer); //вывод на экран количества рядов
                 int sizec = gridPage.getTableCells(id); // оределить количество столбцов в открывшейся таблице
-              //  JOptionPane.showMessageDialog(null, "cells ="+sizec); //вывод на экран количества столбцов
 
-                String xpathofwrapper = "//*[@id='main-vir-sections']/li[" +num1+"]//li[" +num+"]/div[3]/div[@class='expand-wrapper']";
 
                 int row =2; //начинаем со второго ряда - там первая ячейка с полем инпут
                do {
                    num2 = Integer.toString(row); // переводим номер ряда в строку для использования при  формировании хpath ячейки
-                   WebElement Element = gridPage.cell(id, num2, "2");
-                   action.moveToElement(Element).perform();
-                     JOptionPane.showMessageDialog(null, "move to element");
-                   Thread.sleep(2000);
+                   WebElement cellElement = gridPage.cell(id, num2, "1");
+                   action.moveToElement(cellElement).perform();
+                 //    JOptionPane.showMessageDialog(null, "move to element");
 
                    int cell = 2; // начинаем со второго столбца - там первая ячейка с полем инпут
                    do {
 
                        num3 = Integer.toString(cell); // переводим номер столбца в строку для использования при  формировании хpath ячейки
-                       //   JOptionPane.showMessageDialog(null, "row and cell =" + row + "and" + cell);
-
-                       WebElement cellElement = gridPage.cell(id, num2, num3); // вебэлемент -ячейка
+                       cellElement = gridPage.cell(id, num2, num3); // вебэлемент -ячейка
                         String title = cellElement.getAttribute("title"); // получаем атрибут ячейки "title"
                          boolean titleEmpty = title.isEmpty(); //проверяем если ячейка пустая
 
                        if (titleEmpty != true) {    // если ячейка таблицы не пустая, то
 
-                           Thread.sleep(700);
+                           Thread.sleep(500);
                            cellElement.click(); // кликаем на ячейку
-                           WebElement cellinput = gridPage.inputCell(id, num2, num3);
-                           Thread.sleep(700);
+                         WebElement cellinput = gridPage.inputCell(id, num2, num3);
+                           Thread.sleep(500);
                            cellinput.sendKeys(Keys.ENTER); // кликаем Enter
-
                        }
-
                       cell = cell+1; // колисемтво проёденных ячеек
-
                    } while (cell <= sizec);
 
-
                 row = row+1; // переходим на следующий ряд
-
-
-
-              //     scrollTable3(xpathofwrapper);
-
-
-
                } while (row <= sizer);
 
                 gridPage.exteriorElement(num, num1).click(); //закрываем элемент секции грид, с прокликаной таблицей
                 i = i + 1; // переходим к следующему элементу секции
-
             } while (i < size );
 
            gridPage.virSection(num1).click(); // сворачиваем дерево пройденной секции
-           Thread.sleep(1000);
+           Thread.sleep(400);
            virsection = virsection+1; //переходим к следующей секции
-
         } while (virsection<6); // тест для первых пяти секций грид
     }
 
@@ -152,22 +127,6 @@ public class Grid extends TestBase  {
             }
         };
 
-       private void scrollTable2(WebElement table) {
-           ((JavascriptExecutor)driver).executeScript("table.scrollLeft = 20;");
-
-
-       }
-
-       private WebElement scrollTable(WebElement cellelEment, String xpathofcell) throws InterruptedException {
-
-          // ((JavascriptExecutor)driver).executeScript("('#" +id+ "').scrollLeft(+100);");
-           ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();"
-                   ,cellelEment);
-         cellelEment = (new WebDriverWait(driver, 10))
-                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpathofcell)));
-                 return cellelEment;
-
-       }
 
     // wait for jQuery to load
     ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
@@ -183,12 +142,6 @@ public class Grid extends TestBase  {
         }
     };
 
-    private void scrollTable3( String xpath) throws InterruptedException {
-
-        WebElement element = driver.findElement(By.xpath(xpath));
-
-        ((JavascriptExecutor) driver).executeScript("element.scrollLeft(+100);");
-    }
 
 }
 
