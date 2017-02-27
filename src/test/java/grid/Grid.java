316f96f812e605377c7c87487783276e6d70336b
Irina20017 <imudryakodessa@gmail.com>
2017-02-27 09:14:51 +0200

    vir_grid works sincronicly

--- src/test/java/grid/Grid.java
+++ src/test/java/grid/Grid.java
@@ -5,14 +5,13 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import testcase.TestBase;
 import dmsAuction.AdminGridPage;
 import org.openqa.selenium.Keys;
-
-import javax.swing.*;
 import java.util.List;
 
 
@@ -34,32 +33,29 @@ public class Grid extends TestBase  {
 
 
 
+
     @BeforeClass
     public void openGridPage() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         loginPage.loginToDms();
-        wait.until(jsLoad);
-     //   Thread.sleep(1000);
+        waitForJSandJQueryToLoad();
         driver.get(baseUrl+"/dms/auctions/admin#grading");
         gridPage = PageFactory.initElements(driver, AdminGridPage.class);
-        wait.until(jsLoad);
-        wait.until(jQueryLoad );
-      //  Thread.sleep(1000);
-
+        waitForJSandJQueryToLoad();
 
     }
 
     @Test(groups = "dms")
     public void exteriorCheck() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
         Actions action = new Actions(driver);
         int virsection = 1;
 
        do {
-           Thread.sleep(1000);
+
             num1 = Integer.toString(virsection);
             gridPage.virSection(num1).click();  // selected Vir section
-            Thread.sleep(2000);
-
+            waitForJSandJQueryToLoad();
 
 
            int size = gridPage.elementsInVirSection(num1).size(); //определить количество элементоа в выпадающем списке VIR секции
@@ -68,8 +64,11 @@ public class Grid extends TestBase  {
             do {
 
                 num = Integer.toString(i);
-                gridPage.exteriorElement(num, num1).click(); //кликнуть на элемент VIR секции
-                Thread.sleep(500);
+                WebElement gridElement = gridPage.exteriorElement(num, num1);
+                action.moveToElement(gridElement).perform();
+                gridElement.click(); //кликнуть на элемент VIR секции
+                waitForJSandJQueryToLoad();
+
 
                 List<WebElement> tablesList = gridPage.getTableList(num, num1);
                 int sizerT = tablesList.size(); // оределить количество таблиц в <li>
@@ -89,18 +88,18 @@ public class Grid extends TestBase  {
                    int cell = 2; // начинаем со второго столбца - там первая ячейка с полем инпут
                    do {
 
-                       num3 = Integer.toString(cell); // переводим номер столбца в строку для использования при  формировании хpath ячейки
-                       cellElement = gridPage.cell(id, num2, num3); // вебэлемент -ячейка
+                        num3 = Integer.toString(cell); // переводим номер столбца в строку для использования при  формировании хpath ячейки
+                        cellElement = gridPage.cell(id, num2, num3); // вебэлемент -ячейка
                         String title = cellElement.getAttribute("title"); // получаем атрибут ячейки "title"
                          boolean titleEmpty = title.isEmpty(); //проверяем если ячейка пустая
 
                        if (titleEmpty != true) {    // если ячейка таблицы не пустая, то
 
-                           Thread.sleep(500);
+                           waitForJSandJQueryToLoad();
                            cellElement.click(); // кликаем на ячейку
                          WebElement cellinput = gridPage.inputCell(id, num2, num3);
-                           Thread.sleep(500);
                            cellinput.sendKeys(Keys.ENTER); // кликаем Enter
+                           waitForJSandJQueryToLoad();
                        }
                       cell = cell+1; // колисемтво проёденных ячеек
                    } while (cell <= sizec);
@@ -110,37 +109,44 @@ public class Grid extends TestBase  {
 
                 gridPage.exteriorElement(num, num1).click(); //закрываем элемент секции грид, с прокликаной таблицей
                 i = i + 1; // переходим к следующему элементу секции
-            } while (i < size );
+            } while (i <= size );
 
            gridPage.virSection(num1).click(); // сворачиваем дерево пройденной секции
-           Thread.sleep(400);
+           waitForJSandJQueryToLoad();
            virsection = virsection+1; //переходим к следующей секции
         } while (virsection<6); // тест для первых пяти секций грид
     }
 
 
+    public boolean waitForJSandJQueryToLoad() {
 
-        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        // wait for jQuery to load
+        ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
             @Override
             public Boolean apply(WebDriver driver) {
-                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+                try {
+                    return ((Long)((JavascriptExecutor)driver).executeScript("return jQuery.active") == 0);
+                }
+                catch (Exception e) {
+                    // no jQuery present
+                    return true;
+                }
             }
         };
 
+        // wait for Javascript to load
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor)driver).executeScript("return document.readyState")
+                        .toString().equals("complete");
+            }
+        };
 
-    // wait for jQuery to load
-    ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
-        @Override
-        public Boolean apply(WebDriver driver) {
-            try {
-                return ((Long)((JavascriptExecutor)driver).executeScript("return jQuery.active") == 0);
-            }
-            catch (Exception e) {
-                // no jQuery present
-                return true;
-            }
-        }
-    };
+        return wait.until(jQueryLoad) && wait.until(jsLoad);
+    }
 
 
 }
